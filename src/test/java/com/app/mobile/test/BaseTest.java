6c4cb9bddd46dafc281bf3ef8b93aae39bb90a3b
Meenakshi Singh <meenakshi.singh@3pillar.corp>
2017-09-01 13:13:30 +0530

    appium server coding

--- src/test/java/com/app/mobile/test/BaseTest.java
+++ src/test/java/com/app/mobile/test/BaseTest.java
@@ -15,17 +15,21 @@ import io.appium.java_client.service.local.AppiumServiceBuilder;
 
 public abstract class BaseTest {
 	 public static AndroidDriver<MobileElement> driver;
-    String appiumServiceUrl="http://127.0.0.1:4723/wd/hub";
+    //String appiumServiceUrl="http://127.0.0.1:4723/wd/hub";
+    String Appium_Node_Path="C:\\Program Files\\nodejs\\node.exe";
+    String Appium_JS_Path="C:\\Program Files (x86)\\Appium\\node_modules\\appium\\bin\\appium.js";
     
 	@BeforeSuite(alwaysRun=true)
-	public void setUp() throws MalformedURLException{
-		/*AppiumDriverLocalService appiumService = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
-		.usingDriverExecutable(new File ("C:\\Program Files\\nodejs\\node.exe"))
-		.withAppiumJS(new File ("C:\\Program Files (x86)\\Appium\\node_modules\\appium\\bin\\appium.js"))
+	public void setUp() throws MalformedURLException, Exception {
+		AppiumDriverLocalService appiumService = AppiumDriverLocalService.buildService(new AppiumServiceBuilder()
+		.usingDriverExecutable(new File (Appium_Node_Path))
+		.withAppiumJS(new File (Appium_JS_Path))
 		.withLogFile(new File("D:\\apiumlogs\\logs.txt")));
 		appiumService.start();
-        appiumServiceUrl = appiumService.getUrl().toString();
-        System.out.println("Appium Service Address : - "+ appiumServiceUrl);*/
+		Thread.sleep(2000);
+        //String appiumServiceUrl = appiumService.getUrl().toString();
+		String appiumServiceUrl="http://127.0.0.1:4723/wd/hub";
+        System.out.println("Appium Service Address : - "+ appiumServiceUrl);
 		
 	File classpathRoot = new File(System.getProperty("user.dir"));
 	File appDir = new File(classpathRoot, "/Apps/IAmBanking/");
